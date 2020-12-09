dac211c1d591d1bd14c8daa2dcda5cc6cd4e1156
Steven Mancine <stevenrmancine@gmail.com>
2020-12-09 11:29:22 -0700

    Fix broken image bug, reassign currentCover variable

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-document.getElementsByClassName('cover-image')[0].src = covers[getRandomIndex(covers)];
+var currentCover = document.querySelector('.cover-image').src = covers[getRandomIndex(covers)];
+
 
 
 
