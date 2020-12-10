ce6d5cfbab88cebdd25048d50808241d80acb31b
Steven Mancine <stevenrmancine@gmail.com>
2020-12-09 23:45:23 -0700

    Add saved cover button features

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ var formView = document.querySelector('.form-view');
 var mainCover = document.querySelector('.main-cover');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedCoversButton = document.querySelector('.view-saved-button')
+var savedCoversSection = document.querySelector('.saved-view')
+var homeButton = document.querySelector('.home-button')
 
 // We've provided a few variables below
 var savedCovers = [
@@ -13,15 +16,25 @@ var currentCover;
 
 // Add your event listeners here 👇
 makeNewCover.addEventListener('click', changeToMakeNewCover);
+viewSavedCoversButton.addEventListener('click', changeToSavedCovers);
 
 // Create your event handlers and other functions here 👇
-function changeToMakeNewCover(){
-formView.className = 'form-view';
-mainCover.className = 'view main-cover hidden';
-randomCoverButton.className = 'view random-cover-button hidden';
-saveCoverButton.className = "view save-cover-button hidden";
-
+function changeToSavedCovers(){
+  homeButton.className = 'home.button'
+  savedCoversSection.className = 'saved-view'
+  formView.className = 'view form-view hidden'
+  mainCover.className = 'view main-cover hidden';
+  saveCoverButton.className = 'view save-cover-button hidden'
+  randomCoverButton.className = 'view random-cover-button hidden';
 }
+
+function changeToMakeNewCover(){
+  homeButton.className = 'home.button'
+  formView.className = 'form-view';
+  mainCover.className = 'view main-cover hidden';
+  randomCoverButton.className = 'view random-cover-button hidden';
+  saveCoverButton.className = "view save-cover-button hidden";
+};
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
