a40006c8f342871c0274675307e0aede20a83727
Steven Mancine <stevenrmancine@gmail.com>
2020-12-10 12:08:46 -0700

    Add three buttons with refactored code for menu bar

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ var viewSavedCoversButton = document.querySelector('.view-saved-button')
 var savedCoversSection = document.querySelector('.saved-view')
 var homeButton = document.querySelector('.home-button')
 
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -17,24 +18,34 @@ var currentCover;
 // Add your event listeners here 👇
 makeNewCover.addEventListener('click', changeToMakeNewCover);
 viewSavedCoversButton.addEventListener('click', changeToSavedCovers);
+homeButton.addEventListener('click', goHome);
 
 // Create your event handlers and other functions here 👇
 function changeToSavedCovers(){
-  homeButton.className = 'home.button'
-  savedCoversSection.className = 'saved-view'
-  formView.className = 'view form-view hidden'
-  mainCover.className = 'view main-cover hidden';
-  saveCoverButton.className = 'view save-cover-button hidden'
-  randomCoverButton.className = 'view random-cover-button hidden';
-}
+  homeButton.style.display = 'block';
+  savedCoversSection.style.display = 'block'
+  formView.style.display = 'none';
+  mainCover.style.display = 'none';
+  saveCoverButton.style.display = 'none';
+  randomCoverButton.style.display = 'none';
+};
 
 function changeToMakeNewCover(){
-  homeButton.className = 'home.button'
-  formView.className = 'form-view';
-  mainCover.className = 'view main-cover hidden';
-  randomCoverButton.className = 'view random-cover-button hidden';
-  saveCoverButton.className = "view save-cover-button hidden";
+  formView.style.display = 'block';
+  homeButton.style.display = 'block';
+  mainCover.style.display = 'none';
+  randomCoverButton.style.display = 'none';
+  saveCoverButton.style.display = 'none';
 };
+
+function goHome() {
+  mainCover.style.display = 'block'
+  saveCoverButton.style.display = 'block'
+  randomCoverButton.style.display = 'block'
+  homeButton.style.display = 'none'
+  formView.style.display = 'none'
+  savedCoversSection.style.display = 'none'
+}
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
