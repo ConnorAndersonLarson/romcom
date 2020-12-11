25e4c0486cadcf3b19d492c3b7bf4be4c9f104b9
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2020-12-10 21:35:38 -0600

    Add create cover functionality

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var currentCover;
+var currentCover// = savedCovers[(savedCovers.length)]
 // Add your event listeners here 👇
 randomCoverButton.addEventListener('click', randomCover);
 makeNewCover.addEventListener('click', changeToMakeNewCover);
@@ -42,22 +42,25 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
+function currentCover(array) {
+  selectedTitle.innerText = array[0];
+  selectedCover.src = array[1];
+  taglineTwo.innerText = array[3];
+    if (array[2] === array[2]) {
+      randomDescOne = descriptors[getRandomIndex(descriptors)]
+      taglineOne.innerText = array[2];
+   } else {
+      taglineOne.innerText = array[2];
+   }
+}
 
 function randomCover(){
     var randomDescTwo = descriptors[getRandomIndex(descriptors)]
     var randomDescOne = descriptors[getRandomIndex(descriptors)]
     var randomCover = covers[getRandomIndex(covers)]
     var randomTitle = titles[getRandomIndex(titles)]
-  selectedTitle.innerText = randomTitle;
-  selectedCover.src = randomCover;
-  taglineTwo.innerText = randomDescTwo;
-    if (randomDescOne === randomDescTwo) {
-      randomDescOne = descriptors[getRandomIndex(descriptors)]
-      taglineOne.innerText = randomDescOne;
-   } else {
-      taglineOne.innerText = randomDescOne;
-   }
-   currentCover = new Cover(randomCover,randomTitle, randomDescOne, randomDescTwo)
+   currentCover([randomTitle, randomCover, randomDescOne, randomDescTwo])
+   //savedCovers.push(randCover);
 }
 randomCover()
 
@@ -89,13 +92,20 @@ function goHome() {
 };
 
 function makeBookCover() {
-  var customCover = new Cover()
-  selectedTitle.innerText = titleInput.value
-  console.log(selectedTitle.innerText)
+
+  var cover = coverInput.value
+  var title = titleInput.value
+  var desc1 = firstDescriptorInput.value
+  var desc2 = secondDescriptorInput.value
+  //console.log(selectedTitle.innerText)
+  customCover = [title, cover, desc1, desc2]
+  currentCover(customCover)
+  goHome()
+  event.preventDefault()
   //should save the data into respective arrays- cover, title, descriprtor
   //Use the values from the inputs to create a new instance of the Cover class
   //Change back to the main home view (hiding the form view again)
   //Display the newly created cover image, title, and descriptors in the main cover
-
+  //selectedTitle.innerText = titleInput.value
 
 };
